1ff0d201647b8056b03adfacf3f49c37dab474a4
sultanvisual9-afk <sultanvisual9@gmail.com>
2026-09-06 19:04:36 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,13 @@
-
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'academy-bg': '#08050D',
+        'academy-purple': '#5A0F8B',
+        'academy-blue': '#00C2FF',
+      },
+    },
+  },
+  plugins: [],
+}
